3aaf17b2098441c33e114b3909a618e53078119b
Dakshita Singh <dakshita.singh2307@gmail.com>
2026-06-25 21:58:10 +0530

    Created dashbords with CSS grid cards

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,32 +1,30 @@
+import "./Dashboard.css";
+
 function Dashboard() {
-    return (
-        <div>
+  return (
+    <>
+      <h1 className="dashboard-title">Dashboard</h1>
 
-            <h1>Dashboard</h1>
+      <div className="dashboard-container">
 
-            <div>
-
-                <h2>Resumes Uploaded</h2>
-                <p>1</p>
-
-            </div>
-
-            <div>
-
-                <h2>Skills Found</h2>
-                <p>4</p>
-
-            </div>
-
-            <div>
-
-                <h2>Career Match</h2>
-                <p>Data Scientist</p>
-
-            </div>
-
+        <div className="dashboard-card resume-card">
+          <h2>Resumes Uploaded</h2>
+          <p>1</p>
         </div>
-    );
+
+        <div className="dashboard-card skills-card">
+          <h2>Skills Found</h2>
+          <p>4</p>
+        </div>
+
+        <div className="dashboard-card career-card">
+          <h2>Career Match</h2>
+          <p>Data Scientist</p>
+        </div>
+
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
